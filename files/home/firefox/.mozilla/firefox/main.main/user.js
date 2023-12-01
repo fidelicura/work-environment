72a8fb2a763e41aae3459f0467b7937d92450613
fidelicura <fidelicura@gmail.com>
2023-12-01 21:24:12 +0300

    changed user agent string in user.js for firefox

--- files/home/firefox/.mozilla/firefox/main.main/user.js
+++ files/home/firefox/.mozilla/firefox/main.main/user.js
@@ -62,7 +62,7 @@ user_pref("privacy.globalprivacycontrol.functionality.enabled", true);
 user_pref("privacy.trackingprotection.enabled",	true);
 user_pref("privacy.trackingprotection.pbmode.enabled", true);
 user_pref("privacy.donottrackheader.enabled", true);
-user_pref("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0");
+user_pref("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.2201.17 Safari/537.36");
 user_pref("privacy.resistFingerprinting", true);
 
 /** OCSP & CERTS / HPKP ***/
